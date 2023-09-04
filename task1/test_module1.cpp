06595c9782d462f67ff9b7095ebe258f81e2f6ce
root <root@DESKTOP-JOAUJG4.localdomain>
2023-09-04 21:24:57 +0700

    add test's file

---
+++ task1/test_module1.cpp
@@ -0,0 +1,11 @@
+#include "module1.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+// TODO: переименовать тесты, написать реализации
+TEST(TaskTest, say_hello) {
+    
+}
+
+}
---
+++ task1/test_module2.cpp
@@ -0,0 +1,11 @@
+#include "module2.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+// TODO: переименовать тесты, написать реализации
+TEST(TaskTest, say_hello) {
+    
+}
+
+}
