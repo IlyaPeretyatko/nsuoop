808a6006d450d9dd9cdc81d36c674667a3b969ff
root <root@DESKTOP-JOAUJG4.localdomain>
2023-09-16 09:14:23 +0700

    delete build

--- task2a/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilya/nsuoop/task2a/build/_deps/googletest-src/googletest
-# Build directory: /home/ilya/nsuoop/task2a/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
