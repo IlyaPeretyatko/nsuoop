f8dd600f01b0ee899ce37307eb479c560f1ad5c9
root <root@DESKTOP-JOAUJG4.localdomain>
2023-12-03 01:43:21 +0700

    delete build

--- SoundProcessor/build/CMakeFiles/corelib.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/corelib.dir/Launch.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/corelib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
