f2a6d96905aa91d2eb74c7fef06390c0533fa89b
ilya <pinkmanilya@mail.ru>
2023-12-25 01:46:02 +0700

    start make iterator

--- parserCSV/main.cpp
+++ parserCSV/main.cpp
@@ -1,13 +1,13 @@
-#include "print_tuple.h"
-//#include "parser_csv.h"
+//#include "print_tuple.h"
+#include "parser_csv.h"
 
 
 int main() {
-  auto tuple = std::make_tuple(1, "dog", 3.14, 5, 7, 'y', 8, "aboba");
-  std::cout << tuple << std::endl;
+//  auto tuple = std::make_tuple();
+//  std::cout << tuple << std::endl;
 
-//    std::ifstream file("test.csv");
-//    CSVParser<int, std::string> parser(file, 0);
+    std::ifstream file("test.csv");
+    CSVParser<int, std::string> parser(file, 0);
     // for (std::tuple<int, std::string> rs : parser) {
     //     std::cout << rs << std::endl;
     // }
--- parserCSV/parser_csv.h
+++ parserCSV/parser_csv.h
@@ -1,30 +1,97 @@
 #pragma once
 
-#include <iostream>
 #include <fstream>
-#include <tuple>
+#include <iostream>
+#include <memory>
+#include <sstream>
 #include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
+template <typename... Args> class CSVParser {
+public:
+  class Iterator;
+
+  CSVParser(std::ifstream &file, size_t countLineSkip, char rowSeparator = '\n',
+            char columnSeparator = ',', char escapeSeparator = '\"')
+      : file_(file), countLineSkip_(countLineSkip), rowSeparator_(rowSeparator),
+        columnSeparator_(columnSeparator), escapeSeparator_(escapeSeparator) {
+    if (!file.is_open()) {
+      // error
+    }
+    currentLine_ = 0;
+    std::string lineSkip;
+    for (size_t i = 0; i < countLineSkip_; ++i) {
+      std::getline(file_, lineSkip, rowSeparator_);
+    }
+  }
+
+private:
+  std::ifstream &file_;
+  size_t countLineSkip_;
+  char rowSeparator_;
+  char columnSeparator_;
+  char escapeSeparator_;
+  size_t currentLine_;
+
+  std::string getLine() {
+    std::string line;
+    getline(file_, line, rowSeparator_);
+    currentLine_++;
+    return line;
+  }
+
+  std::vector<std::string> getTupleOfLine(std::string &line) {
+    std::vector<std::string> tupleVector;
+    std::stringstream str(line);
+    std::string substr;
+    while (std::getline(str, substr, columnSeparator_)) {
+      tupleVector.push_back(substr);
+    }
+    if (tupleVector.size() != sizeof...(Args)) {
+      // error
+    }
+    return tupleVector;
+  }
+};
+
+template <typename... Args> class CSVParser<Args...>::Iterator {
+public:
+  std::tuple<Args...> &operator*() const { return *iter; }
+
+  bool operator==(const Iterator &other) const { return (iter == other.iter); }
+
+  bool operator!=(const Iterator &other) const { return (iter != other.iter); }
+
+private:
+  CSVParser<Args...> &father;
+  std::shared_ptr<std::tuple<Args...>> iter;
+
+  void nextLine() {
+    std::string line = father.getLine();
+    if (line.empty()) {
+      iter = nullptr;
+    } else {
+      iter = std::make_shared<std::tuple<Args...>>(MakeTuple(father.getTupleOfLine(line)));
+    }
+  }
+};
+
+template <typename T> T convert(const std::string str) {
+  std::istringstream iss(str);
+  T value;
+
+  iss >> value;
+  return value;
+}
+
+template <typename ...Args, size_t ...Iters>
+std::tuple<Args...> makeTupleHelper(std::vector<std::string> params, std::index_sequence<Iters...>) {
+  return make_tuple(convert<Args>(params[Iters])...);
+}
 
 template <typename ...Args>
-class CSVParser {
-    public:
-        CSVParser(std::ifstream & file, size_t countLineSkip, char rowSeparator = '\n', char columnSeparator = ',', char escapeSeparator = '\"') :
-        file_(file), countLineSkip_(countLineSkip), rowSeparator_(rowSeparator), columnSeparator_(columnSeparator), escapeSeparator_(escapeSeparator) {
-            if (!file.is_open()) {
-                //error
-            }
-            currentLine_ = 0;
-            std::string lineSkip;
-            for (size_t i = 0; i < countLineSkip_; ++i) {
-                std::getline(file_, lineSkip, rowSeparator_);
-            }
-        }
-    private:
-        std::ifstream & file_;
-        size_t countLineSkip_;
-        char rowSeparator_;
-        char columnSeparator_;
-        char escapeSeparator_;
-        size_t currentLine_;
-};
+std::tuple<Args...> makeTuple(std::vector<std::string> params) {
+  return makeTupleHelper<Args...>(params, std::index_sequence_for<Args...>{});
+}
