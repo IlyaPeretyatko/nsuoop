35bf61506ccaed1c80da3b417975eb4cd3bcd7b2
root <root@DESKTOP-JOAUJG4.localdomain>
2023-12-29 22:12:44 +0700

    add some tests

--- parserCSV/test.cpp
+++ parserCSV/test.cpp
@@ -3,15 +3,68 @@
 #include "gtest/gtest.h"
 
 
-//  auto tuple = std::make_tuple();
-//  std::cout << tuple << std::endl;
-//std::ifstream file("test.csv");
-//CSVParser<int, double> parser(file, 0);
-//for (auto rs : parser) {
-//  std::cout << rs << std::endl;
-//}
 
-TEST(PrintTuple, standart) {
-  EXPECT_EQ(1, 1);
+TEST(PrintTuple, empty) {
+  auto tuple = std::make_tuple();
+  std::stringstream ss;
+  ss << tuple;
+  EXPECT_EQ(ss.str(), "");
+}
 
+TEST(PrintTuple, standart1) {
+  auto tuple = std::make_tuple("aboba", 10, 10.5, 'h', 567);
+  std::stringstream ss;
+  ss << tuple;
+  EXPECT_EQ(ss.str(), "aboba, 10, 10.5, h, 567");
+}
+
+TEST(PrintTuple, standart2) {
+  auto tuple = std::make_tuple(105);
+  std::stringstream ss;
+  ss << tuple;
+  EXPECT_EQ(ss.str(), "105");
+}
+
+TEST(PrintTuple, standart3) {
+  auto tuple = std::make_tuple(105, 105, 105, 105, 105);
+  std::stringstream ss;
+  ss << tuple;
+  EXPECT_EQ(ss.str(), "105, 105, 105, 105, 105");
+}
+
+TEST(PrintTuple, standart4) {
+  auto tuple = std::make_tuple("print", "tuple", 105, 105.222, 105.124);
+  std::stringstream ss;
+  ss << tuple;
+  EXPECT_EQ(ss.str(), "print, tuple, 105, 105.222, 105.124");
+}
+
+TEST(ParserCSV, standart1) {
+  std::ifstream file("../csv_for_tests/test1.csv");
+  std::stringstream ss;
+  CSVParser<int, double> parser(file, 0);
+  for (auto line : parser) {
+    ss << line << std::endl;
+  }
+  EXPECT_EQ(ss.str(), "1, 2\n3, 4\n");
+}
+
+TEST(ParserCSV, standart2) {
+  std::ifstream file("../csv_for_tests/test2.csv");
+  std::stringstream ss;
+  CSVParser<int, double, std::string> parser(file, 0);
+  for (auto line : parser) {
+    ss << line << std::endl;
+  }
+  EXPECT_EQ(ss.str(), "1, 2, aboba\n1, 5.5, wow\n");
+}
+
+TEST(ParserCSV, empty) {
+  std::ifstream file("../csv_for_tests/test3.csv");
+  std::stringstream ss;
+  CSVParser<int, double, std::string> parser(file, 0);
+  for (auto line : parser) {
+    ss << line << std::endl;
+  }
+  EXPECT_EQ(ss.str(), "");
 }
