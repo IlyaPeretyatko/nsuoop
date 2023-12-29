48d52362b1d9e00e4c0f86723affeb25201678c3
root <root@DESKTOP-JOAUJG4.localdomain>
2023-12-29 23:15:18 +0700

    add tests

--- parserCSV/test.cpp
+++ parserCSV/test.cpp
@@ -59,6 +59,16 @@ TEST(ParserCSV, standart2) {
   EXPECT_EQ(ss.str(), "1, 2, aboba\n1, 5.5, wow\n");
 }
 
+TEST(ParserCSV, standart3) {
+  std::ifstream file("../csv_for_tests/test1.csv");
+  std::stringstream ss;
+  CSVParser<int, double, std::string> parser(file, 2);
+  for (auto line : parser) {
+    ss << line << std::endl;
+  }
+  EXPECT_EQ(ss.str(), "");
+}
+
 TEST(ParserCSV, empty) {
   std::ifstream file("../csv_for_tests/test3.csv");
   std::stringstream ss;
@@ -67,4 +77,24 @@ TEST(ParserCSV, empty) {
     ss << line << std::endl;
   }
   EXPECT_EQ(ss.str(), "");
+}
+
+TEST(ParserCSV, improved1) {
+  std::ifstream file("../csv_for_tests/test4.csv");
+  std::stringstream ss;
+  CSVParser<int, std::string, std::string> parser(file, 0, '+', '-', '^');
+  for (auto line : parser) {
+    ss << line << std::endl;
+  }
+  EXPECT_EQ(ss.str(), "1, 2\"hahahaha\", aboba\n1, 5.5, wow\"damn\"\n");
+}
+
+TEST(ParserCSV, improved2) {
+  std::ifstream file("../csv_for_tests/test5.csv");
+  std::stringstream ss;
+  CSVParser<int, int, int, int, int> parser(file, 0, '\n', ';', '^');
+  for (auto line : parser) {
+    ss << line << std::endl;
+  }
+  EXPECT_EQ(ss.str(), "1, 2, 3, 4, 5\n6, 7, 8, 9, 10\n");
 }
