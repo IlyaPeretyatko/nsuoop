9cd3f97c333c4b3dfdfd6576afa89c98c8102b72
ilya <pinkmanilya@mail.ru>
2023-12-29 21:32:28 +0700

    start write tests

---
+++ parserCSV/test.cpp
@@ -0,0 +1,17 @@
+#include "print_tuple.h"
+#include "parser_csv.h"
+#include "gtest/gtest.h"
+
+
+//  auto tuple = std::make_tuple();
+//  std::cout << tuple << std::endl;
+//std::ifstream file("test.csv");
+//CSVParser<int, double> parser(file, 0);
+//for (auto rs : parser) {
+//  std::cout << rs << std::endl;
+//}
+
+TEST(PrintTuple, standart) {
+  EXPECT_EQ(1, 1);
+
+}
