ccf6a2192c2d79da2955eef725ec12668efb551a
root <root@DESKTOP-JOAUJG4.localdomain>
2023-12-30 09:42:18 +0700

    clear main.cpp

--- parserCSV/main.cpp
+++ parserCSV/main.cpp
@@ -2,13 +2,5 @@
 #include "print_tuple.h"
 
 int main() {
-//  auto tuple = std::make_tuple();
-//  std::cout << tuple << std::endl;
-
-    std::ifstream file("test.csv");
-    CSVParser<int, double> parser(file, 0);
-     for (auto rs : parser) {
-         std::cout << rs << std::endl;
-     }
     return 0;
 }
